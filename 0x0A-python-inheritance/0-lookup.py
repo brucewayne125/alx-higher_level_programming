4b9cdbb9ad52ead2989efe9e972a472805dac041
brucewayne125 <snowdbat@gmail.com>
2024-02-05 18:54:48 -0800

    Task0.1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,3 +4,15 @@
 #python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
 def lookup(obj):
     return dir(obj)
+
+class MyClass1(object):
+    pass
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+        pass
+
+print(lookup(MyClass1))
+print(lookup(MyClass2))
+print(lookup(int))
