45c380b1446aba9e035fea8b6b4b13e84155de77
brucewayne125 <snowdbat@gmail.com>
2024-02-07 15:22:54 -0800

    Task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-#python3 -c 'print(__import__("my_module").__doc__)'
-#python3 -c 'print(__import__("my_module").my_function.__doc__)'
-#python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
 def lookup(obj):
-#call the dir() function with the obj argument and return the resulting list.
+"""call the dir() function with the obj argument and return the resulting list."""
     return dir(obj)
 
 class MyClass1(object):
