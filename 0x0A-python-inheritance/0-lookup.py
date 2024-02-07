675e7918916bba88bc2d399a9e87a7902382f44a
brucewayne125 <snowdbat@gmail.com>
2024-02-07 15:26:08 -0800

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,15 +2,3 @@
 def lookup(obj):
 """call the dir() function with the obj argument and return the resulting list."""
     return dir(obj)
-
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
--- 0x0A-python-inheritance/tests/0-main.py
+++ 0x0A-python-inheritance/tests/0-main.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-#python3 -c 'print(__import__("my_module").__doc__)'
-#python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-#python3 -c 'print(__import__("my_module").my_function.__doc__)'
-#python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
 lookup = __import__('0-lookup').lookup
 
 class MyClass1(object):
