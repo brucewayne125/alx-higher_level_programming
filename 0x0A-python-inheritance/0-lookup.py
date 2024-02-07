26a9e2c25bf2cf4ed12859c54b094b5c48329840
brucewayne125 <snowdbat@gmail.com>
2024-02-07 15:40:45 -0800

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def lookup(obj):
 """call the dir() function with the obj argument and return the resulting list."""
-    return dir(obj)
+    return dir(obj))
+    """returns a list of the available atributes of the object"""
