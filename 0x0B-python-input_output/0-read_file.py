d68af019011cb1ac800b6c961ce7317a5c48b94e
brucewayne125 <snowdbat@gmail.com>
2024-02-07 18:27:28 -0800

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+
+"""Defines a function that reads a text file (UTF8) and prints it to stdout."""
 def read_file(filename=""):
-    with open(filename, mode="r", encoding="utf-8") as myfile:
+    """prints the textfile in unicode to stdout."""
+    with open(filename, encoding="utf-8") as myfile:
         print(myfile.read(), end="")
