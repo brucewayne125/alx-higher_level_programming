cac956c87e1b09e502971bdc09194f078e2a9cf6
brucewayne125 <snowdbat@gmail.com>
2024-02-07 18:48:49 -0800

    Task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Defines a function that writes a string to a text file (UTF8) and returns the number of characters written."""
+
+
+
+def write_file(filename="", text=""):
+    """ writes a string  in unicode and returns the length of the char"""
+    with open(filename, 'w', encoding='utf-8') as myfile:
+        myfile.write(text)
+        return len(text)
