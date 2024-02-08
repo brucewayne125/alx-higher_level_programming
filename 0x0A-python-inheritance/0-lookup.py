75d78c9376ba183ef9e73538362bb831c3eaa222
brucewayne125 <snowdbat@gmail.com>
2024-02-07 16:05:56 -0800

    Task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-# lookup() function that calls the dir function with object as an argument
+
+"""
+lookup() function that calls the dir function with object as an argument
+"""
 def lookup(obj):
-    #returns possible attributes for the obect
-    return dir(obj)
+   """
+   returns possible attributes for the obect
+   """
+   return (dir(obj))
