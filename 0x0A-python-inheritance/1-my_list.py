384c833c494c368266b5ce7d0f5d4b49749974f4
brucewayne125 <snowdbat@gmail.com>
2024-02-07 16:47:40 -0800

    Task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,4 +3,5 @@ class MyList(list):
     """print_sorted: Dfines a subclass, MyList, of an inherited class, list"""
     def print_sorted(self):
         """prints a sorted list in ascending order"""
+        sorted_list = sortedself)
         print(sorted(self))
