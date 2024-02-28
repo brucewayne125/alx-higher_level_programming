8b183489573df01aeaa48b17d26c328a7b281dc2
brucewayne125 <snowdbat@gmail.com>
2024-02-28 13:21:17 -0800

    Task101

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def reverse_alphabet():
-    for i in range(122, 96 -1, -2):
+    for i in range(122, 98 -1, -2):
         print("{:c}".format(i) + "{:c}".format(i - 33), end="")
 reverse_alphabet()
