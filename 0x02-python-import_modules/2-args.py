70d4531337a194608e274c94a9ba0cd6186de96e
brucewayne125 <snowdbat@gmail.com>
2024-03-17 14:49:05 -0700

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    num_arg = len(argv) - 1
+    if num_arg < 0:
+        print("{} arguments.".format(num_arg))
+    else:
+        print("{} arguments:".format(num_arg))
+    for i in range(num_arg):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))   
