2e6feca4875ea73274e84049bb5156c156742ab6
brucewayne125 <snowdbat@gmail.com>
2024-03-17 15:00:23 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,7 @@ if __name__ == "__main__":
         print("{} arguments:".format(num_arg))
     else:
         print("{} arguments:".format(num_arg))
-    for i in range(num_arg):
-        print("{}: {:s}".format(i + 1, argv[i + 1]))   
+   
+   for i in range(num_arg):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))
+
