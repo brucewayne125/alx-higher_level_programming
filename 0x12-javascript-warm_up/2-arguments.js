fef1522e21b39a7aa3c8e51c110b593c27ec8685
brucewayne125 <bekelemikias000@gmail.com>
2024-04-08 18:55:17 -0700

    Task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length === 0){
   console('No argument');
-else if (process.argv.length === 1){
+} else if (process.argv.length === 1){
   console('Argument found');
-else {
+} else {
   console('Argument found');
+}
