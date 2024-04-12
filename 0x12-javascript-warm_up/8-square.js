c137a342ea0a7d211019b0f90dc96361ac1e5bea
brucewayne125 <bekelemikias000@gmail.com>
2024-04-11 19:58:36 -0700

    Task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const square = parseInt(process.argv[2]);
+
+if (isNaN(square)) {
+  console.log('Missing size');
+} else {
+    for (let i = 0; i < square; i++) {
+      console.log('x'.repeat(square));
+    }
+  }
